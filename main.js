028317ce57c36bedbb5f5fdd8350532b57d377fa
Xenostar <157074608+Xenostar1337@users.noreply.github.com>
2024-02-27 11:20:18 -0700

    attempt to fix aspect ratios

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ const password = document.getElementById("password");
 const passwordConfirm = document.getElementById("passwordConfirm");
 
 function passwordCheck(){
-    if (password !== passwordConfirm){
+    if (password.value !== passwordConfirm.value){
         error.style.visibility = 'visible';
-    } 
+        console.log(password.value);
+        console.log(passwordConfirm.value);
+    } else {
+        error.style.visibility = "hidden";
+    }
 }
 
 submitButton.addEventListener('click', passwordCheck);
